9d3cd87dbac79f91a4e8907e010070dc8acee451
Alex Leontiev <nailbiter@dtws-work.in>
2019-10-12 22:24:56 +0900

    Updates

---
+++ precache-manifest.8d53de8965fce478a7083498d8ee0b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560f483674ee9e9bb0df9d30ca39f4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe297def8cbdc4e09e6",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "b5a1f9b426b7afc0bb31",
+    "url": "/static/js/2.76b68eee.chunk.js"
+  },
+  {
+    "revision": "5fe297def8cbdc4e09e6",
+    "url": "/static/js/main.6e388cfe.chunk.js"
+  },
+  {
+    "revision": "af8ade3406c08362fc88",
+    "url": "/static/js/runtime-main.8baee16b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
